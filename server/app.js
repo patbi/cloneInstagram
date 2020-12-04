c4f67e2b128a1aae2977a0989ce5fbb0ae16e6b0
patrickbiyaga <devbiyagapatrick@gmail.com>
2020-12-04 06:47:05 +0100

    add middleware

--- server/app.js
+++ server/app.js
@@ -2,9 +2,20 @@ const express = require('express');
 const app = express();
 const PORT = 5000
 
+const custumMiddleware = (req, res, next) => {
+	console.log("Middleware executed!!!")
+	next()
+}
 
 app.get('/', (req, res) => {
+	console.log("home")
 	res.send("Hello world")
+})
+
+
+app.get('/about', custumMiddleware, (req, res) => {
+	console.log("about")
+	res.send("about page")
 })
 
 app.listen(PORT, () => {
